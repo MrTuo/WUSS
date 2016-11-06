bd97127e9e7beff22d4d207452c9842bdfe3a292
AloeyGranka <469500514@qq.com>
2016-11-06 18:47:15 +0800

    不知道为什么提示要我commit一次

--- WUSS/url_manage/views.py
+++ WUSS/url_manage/views.py
@@ -1,5 +1,32 @@
 # coding=utf-8
+from __future__ import unicode_literals
+
 from django.shortcuts import render
+from url_manage.models import Urls
+import datetime
+from django.http import HttpResponseRedirect
 
-# Create your views here.
 
+
+def add_url(request):
+    if request.method == 'POST':
+        url = request.POST.get('url', '')
+        title= request.POST.get('title', '')
+        now = datetime.datetime.now().strftime("%Y-%m-%d %H:%I:%S")
+        update_fq =request.POST.get('update_fq', '1')
+        track_status = request.POST.get('track_status', 'True')
+        push_status = request.POST.get('push_status', 'True')
+        #user = request.user
+        new_url = Urls(url=url, last_check_time=now, update_fq=update_fq,track_status=track_status, push_status= push_status,title=title)
+        new_url.save()
+        return HttpResponseRedirect('/')
+    return render(request, 'add_url.html')
+
+def delete_url(request, urlid):
+    p = Urls.objects.get(id=urlid)
+    p.delete()
+    return HttpResponseRedirect('/')
+
+def show_url(request):
+    urls= Urls.objects.all()
+    return render(request, "show_urls.html", {'urls':urls})
